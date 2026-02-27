1a1f87cb4ce71fdb1cee14033bbb33996ffb0e8d
batstroke <2967865418@qq.com>
2026-02-27 16:34:54 +0800

    chore: clean local artifacts and tighten ignore rules

--- start.bat
+++ start.bat
@@ -3,6 +3,6 @@ setlocal EnableExtensions
 cd /d "%~dp0"
 
 REM Keep the window open on double-click so users can see errors.
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0start.ps1"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0_misc_root\\start.ps1"
 echo.
 pause
